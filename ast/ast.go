9acdc40064dd118b55466f3adfccb455fad66e1b
Benjamin Khong <benkhong27@gmail.com>
2026-06-14 01:51:27 -0700

    implemented return statements for parser

--- ast/ast.go
+++ ast/ast.go
@@ -44,3 +44,11 @@ type Identifier struct {
 
 func (i *Identifier) ExpressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
